69e19eedb89c29a69984e000d5abd18978e6a1b5
walkskyer <walkskyer@qq.com>
2016-05-03 15:28:30 +0800

    修复构建脚本错误

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 go build -o build/proxy-client-64.exe src/asocks_local/local.go
 CGO_ENABLED=0 GOOS=windows GOARCH=386  go build -o build/proxy-client-32.exe src/asocks_local/local.go
-go build -o build/proxy-server-64-win.exe src/asocks_local/local.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64  go build -o build/proxy-server-64-linux src/asocks_local/local.go
+go build -o build/proxy-server-64-win.exe src/asocks_server/server.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64  go build -o build/proxy-server-64-linux src/asocks_server/server.go
